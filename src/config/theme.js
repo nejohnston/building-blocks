c4058e29b34fe71be6c284f44b7bc048a4108e38
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-06-29 16:22:35 -0700

    added reset css to index.css

---
+++ src/config/theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core';
+
+export default createMuiTheme({
+	palette: {
+		textColor: blueGrey900,
+		alternateTextColor: white,
+		primaryColor: '#66BB6A',
+		accentColor: blueGrey900
+	}
+});
